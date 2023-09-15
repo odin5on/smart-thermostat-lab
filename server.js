c75b8f6a04512d5cec23dd1ca7f33200b63a1e30
odin5on <108775964+odin5on@users.noreply.github.com>
2023-09-14 19:09:48 -0500

    data generated by arduino goes to frontend

--- server.js
+++ server.js
@@ -1,12 +1,21 @@
 const express = require('express');
-// import express from 'express'
 const app = express();
+
 const http = require('http');
-// import http from 'http'
 const server = http.createServer(app);
+
 const { Server } = require('socket.io');
 const io = new Server(server);
+
 const smoothie = require('smoothie')
+
+const { SerialPort, ReadlineParser } = require('serialport');
+
+// Configure the serial port
+const port = new SerialPort({port: '/dev/cu.usbmodem14101', baudRate: 9600, path: '/dev/cu.usbmodem14101', autoOpen: true}); // Replace 'COMX' with your Arduino's serial port
+
+// Create a parser to read lines from the Arduino
+const parser = port.pipe(new ReadlineParser({ delimiter: '\n' }));
 
 app.use(express.static('./'))
 
@@ -18,7 +27,17 @@ server.listen(3000, () => {
   console.log('listening on *:3000');
 });
 
-setInterval(function () {
-  io.emit('graph new data', {'time': Date.now(), 'value': Math.random()})
-  console.log('emitting value');
-}, 1000);
+port.on('open', () => {
+  console.log('A device connected');
+});
+
+port.on('close', () => {
+  console.log('A device disconnected');
+});
+
+
+parser.on('readable', function () {
+  var data = parser.read()
+  io.emit('graph new data', {'time': Date.now(), 'value': data});
+  console.log('Data:', data);
+})
